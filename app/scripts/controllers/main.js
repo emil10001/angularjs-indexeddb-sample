c02d6271955d1044cf61ec8001b70a9af190f6f0
John F <ejohn@feigdev.com>
2013-06-23 19:52:54 -0700

    now using idbwrapper lib

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,36 @@
 'use strict';
 
 angular.module('indexedExApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('MainCtrl', function ($scope) {
+    $scope.items = '';
+
+    var initCallback = function(){
+        getItems();
+    };
+
+    var dataStore = new IDBStore('todos', initCallback);
+
+    var getItemsSuccess = function(data){
+        $scope.items = data;
+        // http://jimhoskins.com/2012/12/17/angularjs-and-apply.html 
+        $scope.$apply(); 
+    };
+
+    var errorCallback = function(){
+        console.log('error'); 
+    };
+
+    var getItems = function(){
+        dataStore.getAll(getItemsSuccess,errorCallback);
+        console.log('getItems'); 
+    };
+
+    $scope.deleteItem = function(item){
+        dataStore.remove(item,getItems,errorCallback);
+    }
+
+    $scope.addItem = function(){
+        dataStore.put({'timeStamp': new Date().getTime(), 'text' : $scope.itemname},getItems,errorCallback); 
+        $scope.itemname = ''; 
+    };
+});
